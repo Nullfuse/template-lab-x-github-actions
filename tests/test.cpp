c0a09f210e38ee5b6de277eb76483664ff4daf71
Nullfuse <cbrian2181@gmail.com>
2021-10-18 01:08:37 -0700

    Fixed Bug in test.cpp
    *Fixed bug where rectangle's area member function is being called instead of perimeter function"

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,4 @@
-//#include "../header/rectangle.hpp"
+#include "../header/rectangle.hpp"
 #include "../src/rectangle.cpp"
 
 #include "gtest/gtest.h"
@@ -36,18 +36,18 @@ TEST(Perimeter, Basic_Perimeter_Test) {
     Rectangle rect8;
     rect8.set_width(5);
     rect8.set_height(3);
-    EXPECT_EQ(rect8.area(), 16);
+    EXPECT_EQ(rect8.perimeter(), 16);
 
     Rectangle rect9;
     rect9.set_width(1);
     rect9.set_height(1);
-    EXPECT_EQ(rect9.area(), 4);
+    EXPECT_EQ(rect9.perimeter(), 4);
 
     Rectangle rect10(4, 44);
-    EXPECT_EQ(rect10.area(), 96);
+    EXPECT_EQ(rect10.perimeter(), 96);
 
     Rectangle rect11(1, 0);
-    EXPECT_EQ(rect11.area(), 2);
+    EXPECT_EQ(rect11.perimeter(), 2);
 }
 
 int main(int argc, char **argv) {
